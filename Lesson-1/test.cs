fe4fc5831cafa9bec2a4dd96db3a970562a4dacf
CvdEmnv <emnov.cvd.95@mail.ru>
2023-11-27 15:31:12 +0400

    first lesson

---
+++ Lesson-1/test.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Test									
+{
+
+public class Program
+
+{
+public static void Main()
+{
+Console.WriteLine("Ad daxil et");
+string word=Console.ReadLine();
+Console.WriteLine ("hello" + word);
+Console.ReadLine();
+    }
+  }
+}
