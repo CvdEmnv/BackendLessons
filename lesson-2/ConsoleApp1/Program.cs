6c0097f9686d93af3edc82b5c5560f5aea81dc03
CvdEmnv <emnov.cvd.95@mail.ru>
2023-11-29 13:02:48 +0400

    second lesson

---
+++ lesson-2/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Include Name");
+            //var name = Console.ReadLine();
+            //Console.WriteLine(name + " " + "Hello");
+
+            //Console.WriteLine("insert your age");
+            //int num1=Convert.ToInt32(Console.ReadLine());
+
+            //if (num1>18)
+            //{
+            //    Console.WriteLine("Hello");
+
+            //} 
+            //else
+            //{
+            //    Console.WriteLine("You are not 18");
+
+            //}
+
+            Console.WriteLine("Insert first number");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert second number");
+            int num2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert symbol");
+            string symb=Console.ReadLine();
+
+            if (symb == "+")
+            {
+                Console.WriteLine("num1+ num2");
+            }
+            else if(symb == "/")
+            {
+                Console.WriteLine(num1/ num2);
+            }
+            else if(symb=="*")
+            {
+                Console.WriteLine(num1* num2);
+            }
+            else
+            {
+                Console.WriteLine("The symbol is not exsist");
+            }
+            
+            
+        }
+
+    }
+}
